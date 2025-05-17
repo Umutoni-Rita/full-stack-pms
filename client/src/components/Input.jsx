d84959e1a18ab75a397fc2b3c6eab0651ae0f90e
Umutoni-Rita <umurita37@gmail.com>
2025-05-17 23:10:34 +0200

    Fixed a small issue with signup

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,10 +1,11 @@
-const Input = ({ placeholder, onChange, type = "text" }) => {
+const Input = ({ placeholder, onChange, type = "text", name }) => {
     return(
   <input
     className="p-3 m-3 font-semibold bg-orange-50 rounded-md border border-orange-400 placeholder:font-normal text-black  "
     placeholder={placeholder}
     onChange={onChange}
     type={type}
+    name = {name}
   />);
 };
 
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -17,7 +17,7 @@ const Signup = () => {
 
   //handles change in form inputs
   const handleChange = (e) => {
-    setForm({...form, [e.target.type]:e.target.value})
+    setForm({...form, [e.target.name]:e.target.value})
   }
 
   const handleSignup = async (e) => {
@@ -41,9 +41,9 @@ const Signup = () => {
       <div className="flex flex-col items-center border border-orange-400 p-5 m-5 rounded-md shadow-md">
         <h1 className="font-bold text-xl mb-4">REGISTER</h1>
         <form className="flex flex-col" onSubmit={handleSignup}>
-          <Input placeholder="Enter your email" type="email" onChange={handleChange} />
-          <Input placeholder="Enter your preferred username" type="text" onChange={handleChange} />
-          <Input placeholder="Enter your password" type="password" onChange={handleChange} />
+          <Input placeholder="Enter your email" name="email" type="email" onChange={handleChange} />
+          <Input placeholder="Enter your preferred username" name="username" type="text" onChange={handleChange} />
+          <Input placeholder="Enter your password" name="password" type="password" onChange={handleChange} />
           <Button text="Register" />
         </form>
         <p className="text-gray-400 text-sm">Already have an account? <Link to={'/'} className="text-orange-500">Login</Link></p>
